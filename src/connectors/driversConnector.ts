d9d36877c1611d99782306c6bee68434d0364976
emiliosp <emilio.spatola@gmail.com>
2023-01-30 19:03:09 +0100

    wip

--- src/connectors/driversConnector.ts
+++ src/connectors/driversConnector.ts
@@ -1,24 +1,11 @@
 // Let's say these data are in a DB
 import { setTimeout } from 'node:timers/promises';
-import { DriverType } from '../models/Driver.js';
 
-const drivers: DriverType[] = [{
-  id: 1,
-  name: 'Schumacher',
-  contract: 'Ferrari',
-},
-{
-  id: 2,
-  name: 'Pepp',
-  contract: 'Lamborghini',
-},
-{
-  id: 3,
-  name: 'Errant',
-  contract: 'Audi',
-},
-];
 export const getAllDrivers = async () => {
   await setTimeout(10); // Simulate a delay
-  return drivers;
+  return [{
+    id: 8888,
+    name: 'Errant',
+    contract: 'Audi',
+  }];
 };
