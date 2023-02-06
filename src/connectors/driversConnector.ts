b3b78f730957ff08505a8a87c30d067442b115b7
emiliosp <emilio.spatola@gmail.com>
2023-02-06 18:10:10 +0100

    chore

--- src/connectors/driversConnector.ts
+++ src/connectors/driversConnector.ts
@@ -1,11 +1,7 @@
 // Let's say these data are in a DB
-import { setTimeout } from 'node:timers/promises';
 
-export const getAllDrivers = async () => {
-  await setTimeout(10); // Simulate a delay
-  return [{
-    id: 8888,
-    name: 'Errant',
-    contract: 'Audi',
-  }];
-};
+export const getAllDrivers = async () => [{
+  id: 8888,
+  name: 'Errant',
+  contract: 'Audi',
+}];
